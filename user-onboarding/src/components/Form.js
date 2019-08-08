122b298cf2cc2866eeb97b1b1c424b67a1827973
lilyhoratio <lilyhzhou@gmail.com>
2019-08-08 11:47:01 -0700

    created form using formik and used withFormik

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,11 +1,34 @@
 import React from "react";
+import { Form, Field, withFormik } from "formik"; //withFormik hooks Form component with Formik backend and passes state variables down as props
+import * as yup from "yup"; // import all of stuff in yup and bundle into object called yup
 
-const Form = props => {
+const FormComponent = props => {
+  console.log(props);
   return (
-    <form>
-      <input type="text" name="name" placeholder="name" />
-    </form>
+    <Form>
+      <Field type="text" name="name" placeholder="name" />
+      <Field type="email" name="email" placeholder="email" />
+      <Field type="password" name="password" placeholder="password" />
+      <label>
+        <Field type="checkbox" name="tos" />
+        Agree to Terms
+      </label>
+      <button type="submit">Submit</button>
+    </Form>
   );
 };
 
-export default Form;
+// look up partial application and currying to see more like withFormik()
+const FormikForm = withFormik({
+  mapPropsToValues: ({ name, email, password }) => {
+    return {
+      name: name || "",
+      email: email || "",
+      password: password || ""
+    };
+  },
+  // later
+  validationSchema: yup.object().shape({})
+})(FormComponent); // HOC - component that accepts a component as an argument. Similar to Route, which takes component as arg
+
+export default FormComponent;
