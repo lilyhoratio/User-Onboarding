c1356232860eb139c35b52f0d9ce504af4a3ce5a
lilyhoratio <lilyhzhou@gmail.com>
2019-08-08 11:24:26 -0700

    set up of app and form component

---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Form = props => {
+  return (
+    <form>
+      <input type="text" name="name" placeholder="name" />
+    </form>
+  );
+};
+
+export default Form;
